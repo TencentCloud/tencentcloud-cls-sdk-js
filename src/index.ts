c539a2c0cf63a520b5d61aa36ac7f920cd2a758d
farmerx <albertbi@tencent.com>
2021-12-08 15:52:04 +0800

    更新package文件

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+export { PutLogsRequest } from './request/putLogsRequest';
+export { Response } from './response/response';
 export { LogGroup, LogItem, Content } from './common/log';
 export { AsyncClientOptions } from './models/options';
 export { AsyncClient } from './asyncClient';
+
 
